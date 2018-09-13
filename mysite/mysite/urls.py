01bf9b66514c395b1aa87fb827c4e6f82151bd3c
DESKTOP-MMFDERO\User <javergara91@gmail.com>
2018-09-13 15:38:01 -0500

    first commit API blog

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls', namespace="blog")),
 ]
